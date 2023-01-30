41548f6467f732ec01ab3ed2e9832042f98ee601
Alessandro <alessand02.santoro@edu.unife.it>
2023-01-30 15:38:42 +0100

    S1

---
+++ confronto.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define DIM 10
+
+int main(void) {
+    char s1[DIM], s2[DIM];
+    int ris; //negativa se s1 precede s2, nulla se s1=s2, positiva se s2 precede s1
+
+     scanf("%s%s", s1, s2); //leggiamo il valore delle due stringhe
+
+
+    return 0;
+}
